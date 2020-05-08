abe612af1518fd7004639e03f493017f7c0c51de
William Marques <wmarques.pro@gmail.com>
2020-05-08 17:07:13 +0200

    improve head

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,15 +6,15 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s',
+    title: 'La radio de William',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: 'EDM / House / Trance Mix'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
