698d137bbbfb1b63b9f2321a1789bbf4cfc3e53b
William Marques <wmarques.pro@gmail.com>
2020-12-31 16:53:40 +0100

    final nye site

--- pages/index.vue
+++ pages/index.vue
@@ -2,27 +2,65 @@
   <v-row class="hero-container" align="center" justify="center">
     <v-col class="text-center" cols="12">
       <div class="display-3 mb-10">La radio de William</div>
-      <div class="display-1 mb-10">Prochain live: {{ nextStreamDate }}</div>
-      <v-btn
-        fab
-        x-large
-        color="primary"
-        :loading="state === 'loading'"
-        @click="play()"
-      >
-        <v-icon v-if="state === 'stopped'">{{ icons.play }}</v-icon>
-        <v-icon v-if="state === 'playing'">{{ icons.stop }}</v-icon>
-      </v-btn>
-      <v-row align="center" justify="center" class="mt-10">
-        <v-col class="text-center" cols="12" sm="6" md="4">
-          <v-slider
-            v-model="volume"
-            :prepend-icon="icons.volume"
-            min="0"
-            max="100"
-          ></v-slider>
-        </v-col>
-      </v-row>
+      <div v-if="playerState !== 'replay'" class="display-1 mb-10">
+        Live nouvel an <br />
+        23h30 - 00h30
+      </div>
+      <div v-if="playerState === 'replay'" class="display-1 mb-10">
+        Replay du live nouvel an
+      </div>
+      <div v-if="playerState !== 'hidden'">
+        <div class="headline">Ambiance</div>
+        <v-btn-toggle
+          v-model="song"
+          class="mb-10"
+          color="primary"
+          group
+          mandatory
+        >
+          <v-btn value="chill" width="120">
+            Party
+          </v-btn>
+
+          <v-btn value="hard" width="120">
+            Party Hard
+          </v-btn>
+        </v-btn-toggle>
+        <div>
+          <v-btn
+            fab
+            x-large
+            color="primary"
+            :loading="state === 'loading'"
+            @click="play()"
+          >
+            <v-icon v-if="state === 'stopped' && playerState !== 'replay'"
+              >{{ icons.play }}
+            </v-icon>
+            <v-icon v-if="state === 'playing' && playerState !== 'replay'"
+              >{{ icons.stop }}
+            </v-icon>
+            <v-icon v-if="playerState === 'replay'"
+              >{{ icons.download }}
+            </v-icon>
+          </v-btn>
+        </div>
+        <v-row
+          v-if="playerState !== 'replay'"
+          align="center"
+          justify="center"
+          class="mt-10"
+        >
+          <v-col class="text-center" cols="12" sm="6" md="4">
+            <v-slider
+              v-model="volume"
+              :prepend-icon="icons.volume"
+              min="0"
+              max="100"
+            ></v-slider>
+          </v-col>
+        </v-row>
+      </div>
     </v-col>
     <!-- <audio
       id="player"
@@ -58,19 +96,28 @@
 
 <script>
 import format from 'date-fns/format';
-import { parseISO, differenceInSeconds, isBefore } from 'date-fns';
+import {
+  parseISO,
+  differenceInSeconds,
+  isBefore,
+  isWithinInterval,
+  addSeconds
+} from 'date-fns';
 import { fr } from 'date-fns/locale';
-import { mdiPlay, mdiStop, mdiVolumeHigh } from '@mdi/js';
+import { mdiPlay, mdiStop, mdiVolumeHigh, mdiDownload } from '@mdi/js';
+import colors from 'vuetify/es5/util/colors';
 
 export default {
   data: () => {
     return {
+      song: 'chill',
       audio: null,
       playerState: 'hidden',
       icons: {
         play: mdiPlay,
         stop: mdiStop,
-        volume: mdiVolumeHigh
+        volume: mdiVolumeHigh,
+        download: mdiDownload
       },
       volume: 100,
       nextStreamDate: format(parseISO('2020-09-02T19:30:00.000Z'), 'PPp', {
@@ -83,24 +130,52 @@ export default {
   watch: {
     volume() {
       this.audio.volume = this.volume / 100;
+    },
+    song() {
+      this.state = 'stopped';
+      if (this.song === 'chill') {
+        this.$vuetify.theme.currentTheme.primary = colors.blue;
+        this.audio.src =
+          'https://www.mboxdrive.com/Mix%20trkl%20(online-audio-converter.com).mp3';
+      } else {
+        this.$vuetify.theme.currentTheme.primary = colors.red;
+        this.audio.src =
+          'https://www.mboxdrive.com/mix%20hardcore%20(online-audio-converter.com).mp3';
+      }
     }
   },
   mounted() {
     this.audio = document.getElementById('player');
-    this.streamStart = new Date(2020, 11, 31, 1, 30);
-    // this.streamStart = new Date(2020, 11, 31, 23, 30);
+    this.streamStart = new Date(2020, 11, 31, 23, 30);
 
-    setInterval(() => {
-      if (isBefore(this.streamStart, new Date())) {
-        this.playerState = 'hidden';
-      } else {
-        this.playerState = 'stream';
+    this.interval = setInterval(() => {
+      const now = new Date();
+      if (Number.isNaN(this.audio.duration)) {
+        return;
       }
-      console.log(this.playerState);
+      if (isBefore(now, this.streamStart)) {
+        this.playerState = 'hidden';
+      } else if (
+        isWithinInterval(now, {
+          start: this.streamStart,
+          end: addSeconds(this.streamStart, this.audio.duration)
+        })
+      ) {
+        this.playerState = 'stream';
+      } else {
+        this.playerState = 'replay';
+      }
     }, 1000);
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
   },
   methods: {
     play() {
+      if (this.playerState === 'replay') {
+        window.open(this.audio.src);
+        return;
+      }
       if (this.state === 'loading') {
         return;
       }
